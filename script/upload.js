75d6efc0dcb924a25c5a89a4dbe4a3c7962ad477
awei <lwei@meishubao.com>
2022-03-22 10:59:07 +0800

    feat: upload url

--- script/upload.js
+++ script/upload.js
@@ -43,7 +43,7 @@ const upload = (files, prefix, cb) => {
     }
     request.post(
       {
-        url: 'http://172.18.109.153:3005/core/oss/build_upload',
+        url: 'http://core-serve:3000/core/oss/build_upload',
         formData
       },
       function (error, response, body) {
